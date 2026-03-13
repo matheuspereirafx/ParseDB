38d597b3db3fc3c483c68d9f245b81b1da7f2a3e
Paulo Coelho <pahdcpc@gmail.com>
2026-03-13 18:52:28 +0000

    stacks design

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_12_230222) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_13_184732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "chats", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.bigint "stacks_id"
+    t.bigint "stack_id"
     t.string "title"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.index ["stacks_id"], name: "index_chats_on_stacks_id"
+    t.index ["stack_id"], name: "index_chats_on_stack_id"
     t.index ["user_id"], name: "index_chats_on_user_id"
   end
 
@@ -55,7 +55,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_12_230222) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "chats", "stacks", column: "stacks_id"
+  add_foreign_key "chats", "stacks"
   add_foreign_key "chats", "users"
   add_foreign_key "messages", "chats"
 end
