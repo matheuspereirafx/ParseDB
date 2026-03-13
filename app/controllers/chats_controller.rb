ad073ddd0413577cc181860ea0ab5063902d62c5
matheuspereirafx <matheuspereirafx@gmail.com>
2026-03-13 19:06:22 -0300

    Chat mensagens e rotas ligadas

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,10 +1,10 @@
 class ChatsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_stack
   before_action :set_chat, only: [:show]
 
   def index
-    @chats = current_user.chats.order(created_at: :desc)
-    puts "🔍 @chats = #{@chats.inspect}" # Para debug no terminal
+    @chats = @stack.chats.where(user: current_user).order(created_at: :desc)
   end
 
   def show
@@ -17,17 +17,21 @@ class ChatsController < ApplicationController
   end
 
   def create
-    @chat = Chat.new(chat_params)
+    @chat = @stack.chats.new(chat_params)
     @chat.user = current_user
 
     if @chat.save
-      redirect_to @chat, notice: "Chat criado com sucesso!"
+      redirect_to stack_chat_path(@stack, @chat), notice: "Chat criado com sucesso!"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_stack
+    @stack = Stack.find(params[:stack_id])
+  end
 
   def set_chat
     @chat = current_user.chats.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,9 @@ Rails.application.routes.draw do
 
   root "pages#home"
 
-  resources :stacks, only: [ :index, :show ] do
-    resources :chats, only: [ :create ]
-  end
-
-  # Mínimo necessário para chats funcionarem
-  resources :chats, only: [ :index, :show, :new, :create ] do
-    resources :messages, only: [ :create ]
+  resources :stacks, only: [:index, :show] do
+    resources :chats, only: [:create, :index, :show, :new] do
+      resources :messages, only: [:create]
+    end
   end
 end
