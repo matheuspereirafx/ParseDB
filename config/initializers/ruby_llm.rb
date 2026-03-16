d267721ea58283f430fb8e3c61685ae30daeb8c1
Joana Dias <joana.jou@gmail.com>
2026-03-16 09:14:07 -0300

    api do openai adicionada

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,5 +1,6 @@
 # config/initializers/ruby_llm.rb
 RubyLLM.configure do |config|
+  config.openai_api_key = ENV["OPENAI_API_KEY"]
   config.gemini_api_key = ENV["GEMINI_API_KEY"]
 
   # Enable the new Rails-like API
